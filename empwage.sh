abc4469c3b0496fd8bf0934638c7c2501dc6afdb
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-13 15:36:36 +0530

    UC1 added

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to employee wage compution"
+
+isPresent=1
+employeeCheck=$(( RANDOM%2 ))
+
+if [ $employeeCheck -eq $isPresent ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
