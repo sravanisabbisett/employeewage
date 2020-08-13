241547f854f8583d2943e5fb9de423a3c1e71b7a
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-13 16:09:37 +0530

    UC5added

--- empwage.sh
+++ empwage.sh
@@ -4,16 +4,21 @@ echo "Welcome to employee wage compution"
 dailyWagePerHour=20
 is_Full_Time=2
 is_Part_Time=1
-employeeCheck=$(( RANDOM%3 ))
+numOfDays=20
+for (( days=1; days<=$numOfDays; days++))
+do
 
-case $employeeCheck in
+	employeeCheck=$(( RANDOM%3 ))
+	case $employeeCheck in
 			$is_Full_Time)
 					empHrs=8 ;;
 			$is_Part_Time)
 					empHrs=4 ;;
 							*)
 					empHrs=0 ;;
-esac
-
-salary=$(( $dailyWagePerHour*$empHrs ))
-echo "salary is" $salary
+	esac
+	salary=$(( $dailyWagePerHour*$empHrs ))
+	echo "salary is" $salary
+	totalSalary=$(( $totalSalary+$salary ))
+	echo "TotalSalary is" $totalSalary
+done
